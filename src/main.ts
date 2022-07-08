1e9259c6d2d104b975822bb278f37643f13d9425
Sintaul Mahdi Siam <sintaulsiam@gmail.com>
2022-07-08 16:29:23 +0600

    navigation fixed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 const navToggle = document.querySelector('.nav-toggle')!;
 const navMenu = document.querySelector('.nav-menu')!;
 
-navToggle.addEventListener('click', () => {
+
+const showMenu = function (navToggle, navMenu){
   navMenu.classList.toggle('nav-menu-show');
   navToggle.classList.toggle('nav-toggle-active');
 }
+
+
+navToggle.addEventListener('click', () => {
+  showMenu(navToggle, navMenu);
+}
 )
 
+const navLinks = document.querySelectorAll('.nav_link')!;
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    showMenu(navToggle, navMenu);
+  }
+  )
+})
+
+// let IOptions = {
+//   root: null,
+//   rootMargin: '0px',
+//   threshold: 1.0
+// }
+// let observer = new IntersectionObserver((e) => {
+//   console.log(e);
+// }, IOptions);
+
+// observer.observe();
